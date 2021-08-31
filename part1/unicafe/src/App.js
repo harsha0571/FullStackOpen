6903994092260030fcd3ef57b217acf764328eb4
harsha0571 <harsha90571@gmail.com>
2021-08-31 23:05:39 +0530

    1.10 added

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -15,12 +15,26 @@ const Statistics = (props) => {
 
     <div>
       <h1>statistics</h1>
-      <p>   good:    {good}</p>
-      <p>   neutral: {neutral}</p>
-      <p>   bad:     {bad}</p>
-      <p>   total:   {bad + good + neutral}</p>
-      <p>   average: {((good * 1) + (bad * -1)) / (bad + good + neutral)}</p>
-      <p>   positive: {(good) / (good + bad + neutral)}%</p>
+      <StatLine title='good' value={good} />
+      <StatLine title='neutral' value={neutral} />
+      <StatLine title='bad' value={bad} />
+      <StatLine title='total' value={bad + good + neutral} />
+      <StatLine title='average' value={((good * 1) + (bad * -1)) / (bad + good + neutral)} />
+      <StatLine title='postive' value={(good) / (good + bad + neutral)} />
+
+    </div>
+  )
+}
+
+const Btn = (props) => {
+  return (
+    <button onClick={props.handle}>{props.text}</button>
+  )
+}
+const StatLine = (props) => {
+  return (
+    <div>
+      <p> {props.title}:  {props.value} </p>
     </div>
   )
 }
@@ -29,12 +43,22 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const setToGood = (value) => {
+    setGood(value)
+  }
+  const setToBad = (value) => {
+    setBad(value)
+  }
+  const setToNeutral = (value) => {
+    setNeutral(value)
+  }
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => { setGood(good + 1) }} > good </button>
-      <button onClick={() => { setNeutral(neutral + 1) }} > neutral </button>
-      <button onClick={() => { setBad(bad + 1) }} > bad </button>
+      <Btn handle={() => setToGood(good + 1)} text="good" />
+      <Btn handle={() => setToBad(bad + 1)} text="bad" />
+      <Btn handle={() => setToNeutral(neutral + 1)} text="neutral" />
       <Statistics good={good} bad={bad} neutral={neutral} />
 
     </div>
