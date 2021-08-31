21e015ccf49047eb52c9c33f41dfee9237657448
harsha0571 <harsha90571@gmail.com>
2021-08-31 16:31:14 +0530

    exercise 1.2 added

--- part1/courseinfo/src/components/Content.js
+++ part1/courseinfo/src/components/Content.js
@@ -1,17 +1,17 @@
 import React from 'react'
-
+import Part from './Part'
 const Content = (props) => {
+    const part1 = props.part1
+    const part2 = props.part2
+    const part3 = props.part3
+    const exercises1 = props.exercises1
+    const exercises2 = props.exercises2
+    const exercises3 = props.exercises3
     return (
         <div>
-            <p>
-                {props.part1} {props.exercises1}
-            </p>
-            <p>
-                {props.part2} {props.exercises2}
-            </p>
-            <p>
-                {props.part3} {props.exercises3}
-            </p>
+            <Part part={part1} exercise={exercises1} />
+            <Part part={part2} exercise={exercises2} />
+            <Part part={part3} exercise={exercises3} />
         </div>
     )
 }
---
+++ part1/courseinfo/src/components/Part.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Part = (props) => {
+    return (
+        <div>
+            <p>
+                {props.part} {props.exercise}
+            </p>
+        </div>
+    )
+}
+
+export default Part
