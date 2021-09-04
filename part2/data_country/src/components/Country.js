eaeb56629005635fe0e2834acbfce852694af64b
harsha0571 <harsha90571@gmail.com>
2021-09-04 16:18:24 +0530

    2.13 added

--- part2/data_country/src/components/Country.js
+++ part2/data_country/src/components/Country.js
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import { useState } from 'react'
+import CountryInDetail from './CountryInDetail'
 const Country = ({ country }) => {
+    const [toggle, setToggle] = useState(false)
+
+    const showHere = () => {
+        if (toggle) {
+            return (
+                <CountryInDetail country={country} />
+            )
+        }
+    }
     return (
         <div>
-            {country.name}
+            {country.name} <button onClick={() => { setToggle(!toggle) }}> show</button>
+            {
+                showHere()
+            }
         </div>
     )
 }
