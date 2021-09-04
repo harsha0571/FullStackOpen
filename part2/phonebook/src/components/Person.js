25330514938cef5e55df583308ff29b80c4e9038
harsha0571 <harsha90571@gmail.com>
2021-09-04 22:56:02 +0530

    2.17 added

--- part2/phonebook/src/components/Person.js
+++ part2/phonebook/src/components/Person.js
@@ -1,11 +1,19 @@
 import React from 'react'
-
+import services from '../services/Route'
 const Person = ({ person }) => {
+    const delPerson = () => {
+
+        if (window.confirm(`Delete ${person.name}`)) {
+            services.remove(person.id)
+                .then(persons => console.log(persons))
+        }
+
+    }
     return (
 
         <div key={person.name}>
-            {person.name}  {person.number}
-        </div>
+            {person.name}  {person.number} <button onClick={() => { delPerson() }}>delete</button>
+        </div >
 
     )
 }
--- part2/phonebook/src/services/Route.js
+++ part2/phonebook/src/services/Route.js
@@ -15,5 +15,9 @@ const update = (id, newObject) => {
     const request = axios.put(`${baseUrl}/${id}`, newObject)
     return request.then(response => response.data)
 }
-const services = { getAll, create, update }
+const remove = (id) => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(response => response.data)
+}
+const services = { getAll, create, update, remove }
 export default services
